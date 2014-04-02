6b5fbf7d28cc7ff5262e11ddd1ded436e7d32b77
Yoldark34 <yoldark@gmail.com>
2014-04-02 14:11:51 +0200

    Add left turn specification

--- tetris/tetris_spec.rb
+++ tetris/tetris_spec.rb
@@ -2,17 +2,30 @@
 require './tetris'
 
 describe Tetris do
-  it "Turn right" do
-    tetris = Tetris.new
-    a1 = [[0,0,1,0],
-          [0,0,1,0],
-          [0,1,1,0],
-          [0,0,0,0]]
-    a2 = [[0,0,0,0],
-          [0,1,0,0],
-          [0,1,1,1],
-          [0,0,0,0]]
-    tetris.turnRight(a1).should eq(a2)
+    it "Turn right" do
+        tetris = Tetris.new
+        a1 = [[0,0,1,0],
+              [0,0,1,0],
+              [0,1,1,0],
+              [0,0,0,0]]
+        a2 = [[0,0,0,0],
+              [0,1,0,0],
+              [0,1,1,1],
+              [0,0,0,0]]
+        tetris.turnRight(a1).should eq(a2)
+    end
+    it "Turn right" do
+        tetris = Tetris.new
+        a1 = [[0,0,1,0],
+              [0,0,1,0],
+              [0,1,1,0],
+              [0,0,0,0]]
+        a2 = [[0,0,0,0],
+              [1,1,1,0],
+              [0,0,1,0],
+              [0,0,0,0]]
+        tetris.turnLeft(a1).should eq(a2)
   end
+
 end
 
