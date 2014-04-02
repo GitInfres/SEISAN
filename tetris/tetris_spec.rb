75a0c72d510c495d29765e7bab04d536d5f50c9e
Yoldark34 <yoldark@gmail.com>
2014-04-02 14:59:31 +0200

    Reoraganise specs

--- tetris/tetris_spec.rb
+++ tetris/tetris_spec.rb
@@ -2,22 +2,26 @@
 require './tetris'
 
 describe Tetris do
-    it "Move left" do
+    describe Movement do
+        it "Move left" do
         
-    end
-    it "Move right" do
-        
-    end
-    it "Speed down" do
+        end
+        it "Move right" do
+                     
+        end
+        it "Speed down" do
+    
+        end
+        decribe Possibility do
+            it "Can move left" do
 
-    end
-    it "Can move left" do
+            end
+            it "Can move right" do
 
-    end
-    it "Can move right" do
+            end
+            it "Can move down" do
 
-    end
-    it "Can move down" do
-
+            end
+        end
     end
 end
