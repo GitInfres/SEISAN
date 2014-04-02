8cea7a9783b417ef0e4b6b31e3381671a94df566
Yoldark34 <yoldark@gmail.com>
2014-04-02 23:32:41 +0200

    Try to get keyboard input. Not good at all

--- tetris/tetris_controller.rb
+++ tetris/tetris_controller.rb
@@ -1,6 +1,10 @@
 # tetris_controller.rb
 require './tetris.rb'
 require 'timeout'
+require 'thread'
+#require 'io/console'
+require 'curses'
+require 'io/wait'
 
 system "clear"
 
@@ -8,9 +12,10 @@ class Tetris_controller
     def initialize
         @tetris = Tetris.new
         @close = false
+        Thread.new { self.listen_keyboard }
     end
     def start_game
-        puts 'Start tetris'
+        sleep(1)
         self.display_grid
         @time = Thread.new { 
             begin
@@ -18,6 +23,8 @@ class Tetris_controller
                     substract_time = 0.1*@tetris.get_game_speed
                     sleep(1-substract_time.to_f)
                     #@tetris.active_tetra_move_down
+                    system "clear"
+                    self.display_grid
                 end
             end
         }
@@ -34,8 +41,33 @@ class Tetris_controller
             print "\n"
         end
     end
-    def user_input
-        false
+    def char_if_pressed
+        begin
+            c = nil
+            system("stty raw -echo") # turn raw input on
+            if $stdin.ready?
+                c = $stdin.getc
+            end
+            system "stty -raw echo" # turn raw input off
+            c.chr if c
+        ensure
+            system "stty -raw echo" # turn raw input off
+        end
+    end 
+    def listen_keyboard
+        while true
+            c = char_if_pressed
+            self.commands(c)
+            sleep(0.05)
+        end
+    end
+    def commands key
+        case key
+        when "q"
+            @close = true
+        else
+        print key
+        end
     end
     def get_game_status?
         @close
@@ -45,5 +77,5 @@ end
 controller = Tetris_controller.new
 controller.start_game
 while !controller.get_game_status?
-    sleep(2)
+    sleep(1)
 end
