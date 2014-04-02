6e22c7c44f107ebab241304a682cea6a02d86fca
Yoldark34 <yoldark@gmail.com>
2014-04-02 18:33:48 +0200

    Update random tetra test

--- tetris/tetra_spec.rb
+++ tetris/tetra_spec.rb
@@ -19,41 +19,60 @@ describe Tetra do
         tetra.turnLeft().should eq(a2)
     end
     it "Get random tetra" do
-      # tetra j
-      tetra_j = [[0,0,1,0],
-            [0,0,1,0],
-            [0,1,1,0],
-            [0,0,0,0]]
-      # tetra l
-      tetra_l = [[0,0,1,0],
-            [0,0,1,0],
-            [0,0,1,1],
-            [0,0,0,0]]
-      # tetra i
-      tetra_i = [[0,0,1,0],
-            [0,0,1,0],
-            [0,0,1,0],
-            [0,0,1,0]]
-      # tetra carré
-      tetra_c = [[0,0,0,0],
-            [0,1,1,0],
-            [0,1,1,0],
-            [0,0,0,0]]
-      # tetra t
-      tetra_t = [[0,0,0,0],
-            [0,0,1,0],
-            [0,1,1,1],
-            [0,0,0,0]]
-      # tetra s
-      tetra_s = [[0,0,0,0],
-            [0,0,1,1],
-            [0,1,1,0],
-            [0,0,0,0]]
-      # tetra z
-      tetra_z = [[0,0,0,0],
-            [1,1,0,0],
-            [0,1,1,0],
-            [0,0,0,0]]
+        # tetra j
+        tetra_j = [[0,0,1,0],
+                   [0,0,1,0],
+                   [0,1,1,0],
+                   [0,0,0,0]]
+        Tetra.new(0).should eq(tetra_j)
+
+        # tetra l
+        tetra_l = [[0,0,1,0],
+                   [0,0,1,0],
+                   [0,0,1,1],
+                   [0,0,0,0]]
+        
+        Tetra.new(1).should eq(tetra_l)
+
+        # tetra i
+        tetra_i = [[0,0,1,0],
+                   [0,0,1,0],
+                   [0,0,1,0],
+                   [0,0,1,0]]
+        
+        Tetra.new(2).should eq(tetra_i)
+
+        # tetra carré
+        tetra_c = [[0,0,0,0],
+                   [0,1,1,0],
+                   [0,1,1,0],
+                   [0,0,0,0]]
+        
+        Tetra.new(3).should eq(tetra_c)
+
+        # tetra t
+        tetra_t = [[0,0,0,0],
+                   [0,0,1,0],
+                   [0,1,1,1],
+                   [0,0,0,0]]
+        
+        Tetra.new(4).should eq(tetra_t)
+
+        # tetra s
+        tetra_s = [[0,0,0,0],
+                   [0,0,1,1],
+                   [0,1,1,0],
+                   [0,0,0,0]]
+        
+        Tetra.new(5).should eq(tetra_s)
+
+        # tetra z
+        tetra_z = [[0,0,0,0],
+                   [1,1,0,0],
+                   [0,1,1,0],
+                   [0,0,0,0]]
+
+        Tetra.new(6).should eq(tetra_z)
 
       liste_tetras = Array.new()
       liste_tetras.push(tetra_j)
