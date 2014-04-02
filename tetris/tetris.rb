9a96b6de6a074b89a1d2ed7571cc930a731bebcb
Yoldark34 <yoldark@gmail.com>
2014-04-02 19:18:20 +0200

    Improve initialize

--- tetris/tetris.rb
+++ tetris/tetris.rb
@@ -3,6 +3,10 @@
 class Tetris
     def initialize
         @tetris_grid = Array.new(20) { Array.new(10, 0) }
+        @active_tetra = Tetra.new
+        @active_tetra_coord = [0, 3]
+        TETRA_WIDTH 4
+        TETRA_HEIGHT 4
     end
 end
 
